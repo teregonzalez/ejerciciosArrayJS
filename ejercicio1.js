c7072614cb69dfad17397feb70779ba8a7e8f915
teregonzalez <teregonzalezpro@gmail.com>
2022-01-26 23:15:06 -0300

    Se agrega otra funcion para solucionar con for

--- ejercicio1.js
+++ ejercicio1.js
@@ -1,6 +1,17 @@
 const shuffle = (someArray) => {
-    let shuffledArray = []
-    return shuffledArray = someArray.sort((a, b) => 0.5 - Math.random());
+    //let shuffledArray = []
+    //return shuffledArray = someArray.sort((a, b) => 0.5 - Math.random());
+
+    for (let i = someArray.length - 1; i > 0; i--) {
+        //recorro el array en inverso
+        const j = Math.floor(Math.random() * (i + 1));
+        //crear posiciones random
+        const temp = someArray[i];
+        someArray[i] = someArray[j];
+        someArray[j] = temp;
+        //toma un item random y lo reemplaza por el que esta siendo iterado
+      }
+      return someArray;
 }
 
 console.log(shuffle([10, 12, 15])) // [12, 15, 10]
