7b45119168db9b108a34c0985d7a400976ab7749
teregonzalez <teregonzalezpro@gmail.com>
2022-01-28 13:26:53 -0300

    Se completa el ejercicio2

--- ejercicio2.js
+++ ejercicio2.js
@@ -1,14 +1,11 @@
 //2. Evenly mix two arrays
 const mix = (firstArray, secondArray) => {
     let finalString = []
-    let longestString = (firstArray, secondArray) => {
-        firstArray.length > secondArray.length ? longestString.push(firstArray) : longestString.push(secondArray);
-        return longestString
-    }
+    let longestString = firstArray.concat(secondArray)
 
     for (let i = 0; i <= longestString.length; i++) {
-        finalString.push(firstArray[i])
-        finalString.push(secondArray[i])
+        firstArray[i] && finalString.push(firstArray[i])
+        secondArray[i] && finalString.push(secondArray[i])
     }
     return finalString
 }
@@ -18,13 +15,5 @@ console.log(mix([
     1, 2, 3
 ], [4, 5, 6]))
 // [1, 4, 2, 5, 3, 6]
-console.log(mix([
-    "h", "a", "c"
-], [
-    7,
-    4,
-    17,
-    10,
-    48
-]))
+console.log(mix(["h", "a", "c" ], [ 7, 4, 17, 10, 48 ]))
 // ["h", 7, "a", 4, "c", 17, 10, 48]
--- ejercicio3.js
+++ ejercicio3.js
@@ -1,10 +1,8 @@
 //3. Order the arrays by sum, ascending or descending
 const orderBySum = (someArray) => {
     
-    const sumArray= someArray.map(e => e.reduce((a, b) => a + b, 0));
+    const sumArray= someArray.map(e => e.reduce((a, b) => a + b, 0)).sort()
     console.table(sumArray)
-    const orderedArrayn = sumArray.sort()
-    console.table(orderedArrayn)
     console.table(someArray)
 }
 console.log(orderBySum([[1,3], [4,2], [2,1]]))
