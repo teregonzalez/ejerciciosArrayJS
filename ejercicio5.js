0cd9a432b0d2ee501708d8283f8b24ba0b1d48ce
teregonzalez <teregonzalezpro@gmail.com>
2022-01-31 13:59:47 -0300

    Cambios

---
+++ ejercicio5.js
@@ -0,0 +1,7 @@
+//Find the smallest and largest numbers
+const findSmallestAndLargestNumbers = (someArray) => {
+    // your code
+}
+
+console.log(findSmallestAndLargestNumbers([14, 28, 3, 8, 2])) 
+// 2, 28
