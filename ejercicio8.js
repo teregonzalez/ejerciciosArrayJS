5e30a48b6c2f266e4df09e581899680b7ad902fd
teregonzalez <teregonzalezpro@gmail.com>
2022-02-01 15:04:07 -0300

    Cambios y nuevos ejercicios

---
+++ ejercicio8.js
@@ -0,0 +1,12 @@
+//Spell out the alphabet
+const alphabetize = (someArray) => {
+
+}
+
+const jumbledAlphabetically = [
+    ["e", "d", "f"],
+    ["a", "c", "b"],
+    ["m", "o", "n"]
+]
+console.log(alphabetize(jumbledAlphabet))
+// [["a", "b", "c"], ["d", "e", "f"], ["m", "n", "o"]]
