ab6ee0b93d90a6ed48d247db6b69f35e44592c5e
teregonzalez <teregonzalezpro@gmail.com>
2022-02-01 15:03:46 -0300

    Cambios y nuevos ejercicios

---
+++ ejercicio6.js
@@ -0,0 +1,11 @@
+//Split the numeric array by odd/even
+const splitByOddAndEven = (someArray) => {
+    
+    for (let i = 0; i < someArray.length; i++) {
+        if(someArray % 2 == 0){
+
+        }
+}
+
+console.log(splitByOddAndEven([2, 3, 7, 6, 2, 4, 9)) 
+// [[2, 4, 6], [3, 7, 9]]
