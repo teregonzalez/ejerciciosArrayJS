fff0f836a6c1b744c735313ba5c15179721ffd5d
teregonzalez <teregonzalezpro@gmail.com>
2022-02-02 17:06:28 -0300

    Se empieza copdigo12

---
+++ ejercicio12.js
@@ -0,0 +1,6 @@
+//Move the element
+const move = (someArray, firstIndex, secondIndex) => {
+    return someArray.indexOf(firstIndex) = someArray.indexOf(secondIndex)
+}
+
+console.log(move([4, 5, 7], 2, 1)) // [4, 7, 5]
