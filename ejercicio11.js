e7aa6804e86af154993cbdf01df2a46f489babd3
teregonzalez <teregonzalezpro@gmail.com>
2022-02-02 15:11:51 -0300

    Se mejora ejercicio 6 y se agrega 11

---
+++ ejercicio11.js
@@ -0,0 +1,11 @@
+//Shift all numeric values to the beginning
+const sortTheArray = (someArray) => {
+    let numberArray = []
+    let otherArray = []
+    for (let i = 0; i < someArray.length; i++) {
+        
+        }
+    }
+
+console.log(sortTheArray(["b", 6, "a", "q", 7, 2])) 
+// [2, 6, 7, "a", "b", "q"]
